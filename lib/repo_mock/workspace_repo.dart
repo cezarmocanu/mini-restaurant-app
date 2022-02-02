02e88ba13a8c38748815ff36b084efe6cb91cae1
cezarmocanu <cezarmocanu2014@gmail.com>
2022-02-02 18:33:17 +0200

    004 Created workspace mock repo

---
+++ lib/repo_mock/workspace_repo.dart
@@ -0,0 +1,9 @@
+class WorkspaceRepo {
+  static List<Map<String, dynamic>> getAll() {
+    return [
+      {"id": 0, "name": 'Main Kitchen'},
+      {"id": 1, "name": 'Bar'},
+      {"id": 2, "name": 'Deserts Kitchen'}
+    ];
+  }
+}
