bcba0e95e810d2a62d23db6aaa8237797dee8246
cezarmocanu <cezarmocanu2014@gmail.com>
2022-02-06 19:37:56 +0200

    004 Added MenuRepo

---
+++ lib/repo_mock/menu_repo.dart
@@ -0,0 +1,22 @@
+class MenuRepo {
+  static List<Map<String, dynamic>> getAll() {
+    return [
+      {
+        "id": 0,
+        "name": "Special Offers",
+      },
+      {
+        "id": 1,
+        "name": "Main Course",
+      },
+      {
+        "id": 2,
+        "name": "Drinks",
+      },
+      {
+        "id": 3,
+        "name": "Deserts",
+      }
+    ];
+  }
+}
