13d66b5d4819a12a2c7ae481406e6fd325010872
cezarmocanu <cezarmocanu2014@gmail.com>
2022-02-06 19:37:50 +0200

    004 Added EmployeeRepo

---
+++ lib/repo_mock/employee_repo.dart
@@ -0,0 +1,36 @@
+class EmployeeRepo {
+  static List<Map<String, dynamic>> getAll() {
+    return [
+      {
+        "id": 0,
+        "firstName": "Andrei",
+        "lastName": "Lenonard",
+      },
+      {
+        "id": 1,
+        "firstName": "Eugen",
+        "lastName": "Mihalache",
+      },
+      {
+        "id": 2,
+        "firstName": "Clara",
+        "lastName": "Nitu",
+      },
+      {
+        "id": 3,
+        "firstName": "Vlad",
+        "lastName": "Ercu",
+      },
+      {
+        "id": 4,
+        "firstName": "Diana",
+        "lastName": "Pastor",
+      },
+      {
+        "id": 5,
+        "firstName": "Stefan",
+        "lastName": "Vladau",
+      },
+    ];
+  }
+}
