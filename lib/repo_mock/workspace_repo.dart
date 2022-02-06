090078a9889038afb7729afc8c89c40ac3ff811d
cezarmocanu <cezarmocanu2014@gmail.com>
2022-02-06 19:38:22 +0200

    004 Updated WorkspaceREpo

--- lib/repo_mock/workspace_repo.dart
+++ lib/repo_mock/workspace_repo.dart
@@ -1,9 +1,18 @@
 class WorkspaceRepo {
   static List<Map<String, dynamic>> getAll() {
     return [
-      {"id": 0, "name": 'Main Kitchen'},
-      {"id": 1, "name": 'Bar'},
-      {"id": 2, "name": 'Deserts Kitchen'}
+      {
+        "id": 0,
+        "name": 'Main Kitchen',
+      },
+      {
+        "id": 1,
+        "name": 'Bar',
+      },
+      {
+        "id": 2,
+        "name": 'Deserts Kitchen',
+      }
     ];
   }
 }
